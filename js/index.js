56f0be8074bb2cc64f7a32edece919ba696d05ff
Wendy Chen <LFwendy@gmail.com>
2017-08-24 20:50:53 +0800

    release

--- js/index.js
+++ js/index.js
@@ -10,7 +10,19 @@ var app = new Vue({
       this.name_output = this.$refs.name.value
     },
     addValues() {
-      this.add_output = parseInt(this.$refs.n1.value) + parseInt(this.$refs.n2.value)
+      var num1 = parseInt(this.$refs.n1.value);
+      var num2 = parseInt(this.$refs.n2.value);
+
+      // error check: 檢查文字輸入框內是否皆為數字
+      try {
+        if (isNaN(num1) || isNaN(num2)) throw "文字輸入框內皆為數字才能計算結果";
+        this.add_output = num1 + num2
+
+      } catch(err) {
+        var error_message = alert(err);
+        document.getElementById("error_check").innerHTML = error_message;
+      }
+      
     }
   }
 })
